a2714ee4a87f96ff000c2543fcc31dc03fa4769c
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-08-14 15:27:31 -0400

    Fix missing space before environment variable name on command line arguments

--- scripts/DockerBuild.sh
+++ scripts/DockerBuild.sh
@@ -1,5 +1,5 @@
 
 docker build --pull=true  --rm=true -t simpleitk_conda-bld -f imagefiles/Dockerfile .
 
-docker run --storage-opt size=150G --rm -e _USER=$(id -un)  -e _USERID=$(id -u)  -e_GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
-docker run --storage-opt size=150G --rm -e ARCH=32 -e _USER=$(id -un)  -e _USERID=$(id -u)  -e_GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
+docker run --storage-opt size=150G --rm -e _USER=$(id -un)  -e _USERID=$(id -u)  -e _GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
+docker run --storage-opt size=150G --rm -e ARCH=32 -e _USER=$(id -un)  -e _USERID=$(id -u)  -e _GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
