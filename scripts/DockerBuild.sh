79ae1c4865eff66637b3475e803af2304357e330
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-01-03 11:41:58 -0500

    Use local ExternalData in Docker build script

--- scripts/DockerBuild.sh
+++ scripts/DockerBuild.sh
@@ -1,5 +1,11 @@
 
 docker build --pull=true  --rm=true -t simpleitk_conda-bld -f imagefiles/Dockerfile .
 
+
+if [ -n "$ExternalData_OBJECT_STORES" -a -d "$ExternalData_OBJECT_STORES" ] ; then
+    external_data_args="-v ${ExternalData_OBJECT_STORES}:/var/io/.ExternalData -e ExternalData_OBJECT_STORES=/var/io/.ExternalData"
+fi
+
+
 docker run --rm -e _USER=$(id -un)  -e _USERID=$(id -u)  -e _GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
 docker run --rm -e ARCH=32 -e _USER=$(id -un)  -e _USERID=$(id -u)  -e _GROUPID=$(id -g) $external_data_args -v $(pwd):/work/io -t simpleitk_conda-bld
