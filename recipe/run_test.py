cb6b4c070373d5d4f842fe88e751b23644738b32
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-09-17 15:18:38 -0400

    Add testing of IO with external libraries

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,12 @@
 import SimpleITK as sitk
 
-img = sitk.Image(1,1,1,sitk.sitkFloat32)
+img = sitk.Image(10,10,sitk.sitkUInt8)
+
+# test writing the external third party libraries
+sitk.WriteImage(img, "test.jpg")
+sitk.WriteImage(img, "test.png")
+sitk.WriteImage(img, "test.tiff")
+
+
 
 print(sitk.Version())
