b3c388e8665cfac042ce642c874140de57ed8ded
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-03-10 07:30:07 -0400

    Test Conda build on SimpleITK 2.0 development (#44)
    * Remove Python 2.7 builds
    
    * Update build configurations used
    
    Remove 32-bit builds, add Python 3.8, and update OSX image to 10.14.
    
    * Adjust AZP parallel levels
    
    * Explicitly use MS Visual Studio x64 host toolset
    
    * Update to SimpleITK v2.0rc1 tag
    
    Explicitly disable ITK's architecture optimization flags, to a
    portable executable on older systems.
    
    Disable GDCM's use of CoreFoundation
    
    Seaching for the CoreFoundation library does not with the restrictive
    find root path mode created for the conda build environment.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ if [ $ARCH == 32 -a "${OSX_ARCH:-notosx}" == "notosx" ]; then
 fi
 
 BUILD_DIR=${SRC_DIR}/build
-mkdir ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 
@@ -37,18 +37,20 @@ cmake \
     -D BUILD_EXAMPLES:BOOL=OFF \
     -D WRAP_DEFAULT:BOOL=OFF \
     -D WRAP_PYTHON:BOOL=ON \
+    -D "PYTHON_EXECUTABLE:FILEPATH=${PYTHON}" \
+    -D "PYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}" \
+    -D "PYTHON_LIBRARY=${PYTHON_LIBRARY_DIR}" \
     -D SimpleITK_USE_SYSTEM_SWIG:BOOL=ON \
     -D SimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF \
     -D ITK_USE_SYSTEM_JPEG:BOOL=ON \
     -D ITK_USE_SYSTEM_PNG:BOOL=ON \
     -D ITK_USE_SYSTEM_TIFF:BOOL=ON \
     -D ITK_USE_SYSTEM_ZLIB:BOOL=ON \
-    -D "PYTHON_EXECUTABLE:FILEPATH=${PYTHON}" \
-    -D "PYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}" \
-    -D "PYTHON_LIBRARY=${PYTHON_LIBRARY_DIR}" \
+    -D ITK_C_OPTIMIZATION_FLAGS:STRING= \
+    -D ITK_CXX_OPTIMIZATION_FLAGS:STRING= \
+    -D GDCM_USE_COREFOUNDATION_LIBRARY:BOOL=OFF \
     "${SRC_DIR}/SuperBuild"
 
-cmake --build  . --config Release
-
+cmake --build . --config Release
 cd ${BUILD_DIR}/SimpleITK-build/Wrapping/Python
 ${PYTHON} Packaging/setup.py install
