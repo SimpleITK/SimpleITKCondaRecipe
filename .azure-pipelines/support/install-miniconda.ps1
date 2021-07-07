5716f5bb3c3df260c9d9f21dfd0c21466cd9eeb3
Bradley Lowekamp <blowekamp@mail.nih.gov>
2021-07-07 10:29:25 -0400

    Remove Azure Pipeline yaml, Github Actions replaced

--- .azure-pipelines/support/install-miniconda.ps1
+++
@@ -1,8 +0,0 @@
-$InstallDir="C:\miniconda"
-
-Write-Host "`nDownloading Miniconda Installer...`n"
-(New-Object System.Net.WebClient).DownloadFile("https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe", "$pwd\Miniconda_Install.exe")
-
-Write-Host "Installing Miniconda...`n"
-Start-Process Miniconda_Install.exe "/S /AddToPath=0 /D=$InstallDir" -Wait
-
